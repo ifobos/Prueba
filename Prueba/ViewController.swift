d4a111358ee9978df29f28500d3b071e9514fc03
Juan Carlos <juancarlos.garcia.alfaro@gmail.com>
2018-10-13 16:12:20 -0600

    Initial Commit

---
+++ Prueba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prueba
+//
+//  Created by Juan Carlos Garcia Alfaro on 10/13/18.
+//  Copyright © 2018 Juan Carlos Garcia Alfaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
