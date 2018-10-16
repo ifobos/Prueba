b7bd47dbb2ebde583d8150736a9416e00d2b05d5
Juan Carlos <juancarlos.garcia.alfaro@gmail.com>
2018-10-16 14:12:37 -0600

    core location implementation added

---
+++ Prueba/Location/CoreLocationImplementation/CoreLocationLocalizator.swift
@@ -0,0 +1,63 @@
+//
+//  CoreLocationImplementation.swift
+//  Prueba
+//
+//  Created by Juan Carlos Garcia Alfaro on 10/16/18.
+//  Copyright © 2018 Juan Carlos Garcia Alfaro. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+extension LocalizatorFactory {
+    static func newLocalizator() -> LocalizatorProtocol {
+        return CoreLocationLocalizator()
+    }
+}
+
+class CoreLocationLocalizator: NSObject, LocalizatorProtocol {
+    
+    var currentLocation: LocationCoordinate? {
+        didSet {
+            guard let `currentLocation` = currentLocation else {
+                return
+            }
+            for pendingCompletion in pendingCompletions {
+                pendingCompletion(currentLocation)
+            }
+            pendingCompletions.removeAll()
+        }
+    }
+    
+    var locationManager: CLLocationManager
+    
+    var pendingCompletions: [(LocationCoordinate) -> Void] = []
+    
+    override init() {
+        locationManager = CLLocationManager()
+        super.init()
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+    }
+    
+    func currentLocation(completion: @escaping (LocationCoordinate) -> Void) {
+        if let `currentLocation` = currentLocation {
+            completion(currentLocation)
+        } else {
+            pendingCompletions.append(completion)
+        }
+        
+    }
+    
+}
+
+extension CoreLocationLocalizator: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let coordinate = locations.last?.coordinate else {
+            return
+        }
+        currentLocation = LocationCoordinate(latitude: coordinate.latitude, longitude: coordinate.longitude)
+    }
+}
